5b796b57ef0347fd05e73ac5139f2eaf07778d07
ronak <knockronak@gmail.com>
2018-08-04 16:49:23 -0400

    adding constructor for word and letter

---
+++ letter.js
@@ -0,0 +1,25 @@
+var Letter = function(char){
+    this.char = char;
+    this.isGuess = false;
+    this.showChar = function(){
+        if (this.isGuess === true){
+            return this.char;
+        }
+        else{
+            return "_";
+        }
+    };
+
+    this.evaluateChar = function(char){
+        if (char === this.char){
+            this.isGuess = true;
+        }
+    }
+}
+
+var one = new Letter("a");
+console.log(one.char);
+one.evaluateChar("a");
+one.showChar();
+
+module.exports = Letter;
---
+++ word.js
@@ -0,0 +1,30 @@
+var Letter = require("./letter");
+
+var Word = function(){
+    this.sentence = new Array(Letter);
+    this.getSentence  = function (){
+        var sentence = ""
+        for (var eachChar in this.sentence) {
+            // console.log(eachChar);
+            sentence += " " + this.sentence[eachChar].showChar();
+        }
+        return sentence;
+    }
+
+    this.checkChar = function(char){
+        for (var eachChar in this.sentence){
+            this.sentence[eachChar].evaluateChar(char);
+        }
+    }
+
+}
+
+var two = new Word();
+// two.sentence = ["h", "l", "m"];
+two.sentence = [new Letter("a"), new Letter("b"), new Letter("c")];
+console.log(typeof two.sentence);
+// console.log(two.sentence);
+two.checkChar("c");
+console.log(two.getSentence());
+
+module.exports = Word;
